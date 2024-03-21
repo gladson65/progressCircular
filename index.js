bf3298d9fdcc7ea8339340b5a69fdbd5bba1aa54
gladson65 <grauth65@gmail.com>
2024-03-21 11:09:32 +0530

    circular progress bar added

---
+++ index.js
@@ -0,0 +1,57 @@
+
+window.onload = function() {
+
+    let button = document.querySelector(".btn");
+
+    // flag
+    var clicked = 0;
+
+    button.addEventListener("click", function (e) {
+        e.preventDefault();
+        clicked++;
+        if (clicked === 1) {
+            
+            // random number generate
+            let randomNumber = Math.floor(Math.random() * 100)
+
+
+            let proGress = document.querySelector(".progress-value");
+            let progressValue = randomNumber;
+
+            let outer = document.querySelector(".outer");
+
+
+            let start = 0;
+            let end = progressValue;
+
+            let progress = setInterval(() => {
+                start++;
+
+                proGress.textContent = `${start}`;
+
+                if (start <= 25) {
+                    outer.style.background = `conic-gradient(#FF0060 ${start * 3.6}deg, #effaeb34 0deg)`;
+                }
+                else if (start > 25 && start <= 50) {
+                    outer.style.background = `conic-gradient(#FF0060 ${25 * 3.6}deg, #F6FA70 ${start * 3.6}deg, #effaeb34 ${360 - (75 * 3.6)}deg`;
+                }
+                else if (start > 50 && start <= 75) {
+                    outer.style.background = `conic-gradient(#FF0060 ${25 * 3.6}deg, #F6FA70 ${50 * 3.6}deg, #00DFA2 ${start * 3.6}deg, #effaeb34 ${360 - (start * 3.6)}deg`;
+                }
+                else if (start > 75 && start <= 100) {
+                    outer.style.background = `conic-gradient(#FF0060 ${25 * 3.6}deg, #F6FA70 ${50 * 3.6}deg, #00DFA2 ${75 * 3.6}deg, #0079FF ${start * 3.6}deg, #effaeb34 ${360 - (start * 3.6)}deg`;
+                }
+
+
+                if (start == end) {
+                    clearInterval(progress);
+                    clicked = 0;
+                }
+            }, 40)
+        } 
+            
+    });
+
+    
+}
+
